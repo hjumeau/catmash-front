b6ed65dabbcabcb6888dffff7d1b8eff4faf6968
hjumeau <harry.jumeau@gmail.com>
2019-04-09 13:09:47 +0200

    set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './scenes/Home';
+import Ratings from './scenes/Ratings';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/ratings" component={Ratings}/>
+        <Route component={Home}/>
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
